183db5ab2fffa477a855c0c2289a04319315c6f3
gerasimov_rp@icloud.com <gerasimov_rp@icloud.com>
2021-08-10 09:29:22 +0300

    Progress 60%

--- DocumentWorker.cs
+++ DocumentWorker.cs
@@ -20,5 +20,32 @@ namespace Convert
         {
             return "Сохранение документа доступно в версии Про";
         }
+
+        public int ChoiceCommand()
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберете дейсвтвие:\n" +
+                "1. Открыть документ \n" +
+                "2. Редактировать документ \n" +
+                "3. Сохранить документ \n" +
+                "4. Ввести серийный ключ для версии Pro/Expert");
+
+                Console.WriteLine(new string('#', 40));
+
+                int choice = System.Convert.ToInt32(Console.ReadLine());
+
+                if (0 >= choice|| choice >= 5)
+                {
+                    Console.WriteLine("Такой команды нет, попробуйте снова");
+                    continue;
+                }
+                else
+                {
+                    return choice;                    
+                }
+            }
+            
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,34 @@ namespace Convert
 
         static void Main(string[] args)
         {
-                                  
+            Console.WriteLine("Редактор КопиРайтер готов к работе");
+            Console.WriteLine(new string ('_', 40));
 
+            ExpertDocumentWorker workerExpert = new ExpertDocumentWorker();
+            DocumentWorker worker = workerExpert;
+
+            while (true)
+            {
+                int command = worker.ChoiceCommand();
+
+                switch (command)
+                {
+                    case 1:
+                        worker.OpenDocument();
+                        break;
+                    case 2:
+                        worker.EditDocument();
+                        break;
+                    case 3:
+                        worker.SaveDocument();
+                        break;
+                    case 4:
+
+                        break;
+                }
+            }      
+                   
+                                            
 
         }
 
