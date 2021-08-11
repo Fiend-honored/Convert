4fc380c57168e13af90254670d2a7104dcf8b098
gerasimov_rp@icloud.com <gerasimov_rp@icloud.com>
2021-08-11 11:13:53 +0300

    99%

--- DocumentWorker.cs
+++ DocumentWorker.cs
@@ -21,5 +21,49 @@ namespace Convert
         {
             Console.WriteLine("Сохранение документа доступно в версии Про");
         }
+
+        public string Choice()
+        {
+            Console.WriteLine(new string('#', 40));
+            Console.WriteLine("Выберете дейсвтвие:\n" +
+            "1. Открыть документ \n" +
+            "2. Редактировать документ \n" +
+            "3. Сохранить документ \n" +
+            "4. Ввести серийный ключ для версии Pro/Expert");
+            Console.WriteLine(new string('#', 40));
+            int choice = System.Convert.ToInt32(Console.ReadLine());
+
+            string userChoice = null; 
+
+            switch (choice)
+            {
+                case 1:
+                    OpenDocument();
+                    break;
+                case 2:
+                    EditDocument();
+                    break;
+                case 3:
+                    SaveDocument();
+                    break;
+                case 4:
+                    CheckKey key = new CheckKey();
+                    key.CheckEnterKey();
+                    string valueKey = key.statusKey;
+                    if (valueKey == "pro")
+                    {
+                        userChoice = "pro";
+                    } else if (valueKey == "expert")
+                    {
+                        userChoice = "expert";
+                    }
+                    else
+                    {
+                        userChoice = "free";
+                    }                   
+                    break;
+            }
+            return userChoice;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,46 +9,27 @@ namespace Convert
         static void Main(string[] args)
         {
             Console.WriteLine("Редактор КопиРайтер готов к работе");
-            
-            while (true)
-            {
-                DocumentWorker worker = new DocumentWorker();
 
-                Console.WriteLine(new string('#', 40));
-                Console.WriteLine("Выберете дейсвтвие:\n" +
-                "1. Открыть документ \n" +
-                "2. Редактировать документ \n" +
-                "3. Сохранить документ \n" +
-                "4. Ввести серийный ключ для версии Pro/Expert");
-                Console.WriteLine(new string ('#', 40));
-                int choice = System.Convert.ToInt32(Console.ReadLine());
-                switch (choice)
+            DocumentWorker worker = new DocumentWorker();
+            ProDocumentWorker proWorker = new ProDocumentWorker();
+            ExpertDocumentWorker expertWorker = new ExpertDocumentWorker();
+
+            string status = worker.Choice();
+
+            while (true)
+            {                
+                if(status == "pro")
                 {
-                    case 1:
-                        worker.OpenDocument();
-                        break;
-                    case 2:
-                        worker.EditDocument();
-                        break;
-                    case 3:
-                        worker.SaveDocument();
-                        break;
-                    case 4:
-                        CheckKey key = new CheckKey();
-                        key.CheckEnterKey();
-                        string valueKey = key.statusKey;
-                        if(valueKey == "pro")
-                        {
-                            ProDocumentWorker proWorker = (ProDocumentWorker)worker;
-                        }
-                        break;
+                    status = proWorker.Choice();
+                } else if (status == "expert")
+                {
+                    status = expertWorker.Choice();
+                } else
+                {
+                    status = worker.Choice();
                 }
 
-            }
-
-
-
-                                  
+            }                                
 
 
         }
